eaaa3fd2f774b41cc576fd05563604e9f1d6769e
Enilst11 <emil15setiawan@gmail.com>
2023-02-22 22:20:30 +0700

    Editing files

--- backend/src/main.module.ts
+++ backend/src/main.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './service/app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { GlobalModule } from './module/global.module';
 import { AppController } from './Controller/app.controller';
 import { MulterModule } from '@nestjs/platform-express';
+import { GlobalModule } from './Module/global.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -15,8 +15,8 @@ import { MulterModule } from '@nestjs/platform-express';
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE,
       entities: ['dist/entities/*{.ts,.js}'],
+      autoLoadEntities: false,
       synchronize: false,
-      autoLoadEntities: true
     }),
     GlobalModule,
     // to add pict from resto menu photo or others
@@ -25,4 +25,4 @@ import { MulterModule } from '@nestjs/platform-express';
   controllers: [AppController],
   providers: [AppService],
 })
-export class MainModule { }
+export class MainModule {}
