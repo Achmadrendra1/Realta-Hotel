97cb024da55056e66b532887c5496eb2700b63d8
ilmialimudins <ilmi.muhammad@outlook.com>
2023-03-03 08:56:08 +0700

    3500

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,9 +10,12 @@ async function bootstrap() {
   const app = await NestFactory.create(MainModule);
   app.enableCors();
 
-  // untuk membaca folder restomenuphotos di FE 
-  app.use('/restomenuphotos', express.static(path.join(__dirname,'../restomenuphotos')));
-  
+  // untuk membaca folder restomenuphotos di FE
+  app.use(
+    '/restomenuphotos',
+    express.static(path.join(__dirname, '../restomenuphotos')),
+  );
+
   const port = 3501;
   await app.listen(port, () => {
     console.log(`server anda berjalan pada port ${port}`);
