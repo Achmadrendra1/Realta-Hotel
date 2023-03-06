ade93925fe6eb3021fa48cb5f17cbf97126420bb
Mohammad Farid Nurichsan <faridnurichsan>
2023-03-06 19:46:55 +0700

    Upload Photo

--- frontend/src/pages/dashboard/hotel/AddPhoto.tsx
+++ frontend/src/pages/dashboard/hotel/AddPhoto.tsx
@@ -1,63 +1,50 @@
-import withAuth from '@/PrivateRoute/WithAuth';
-import { addFapho } from '@/Redux/Action/Hotel/HotelAction';
-import { UploadOutlined } from '@ant-design/icons';
-import { Button, Form, Upload, UploadProps } from 'antd';
-import { RcFile, UploadFile } from 'antd/es/upload';
-import { useFormik } from 'formik';
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import withAuth from "@/PrivateRoute/WithAuth";
+import { addFapho } from "@/Redux/Action/Hotel/HotelAction";
+import Buttons from "@/components/Button";
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Form, Select, Upload, UploadProps } from "antd";
+import { RcFile, UploadFile } from "antd/es/upload";
+import { useFormik } from "formik";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 
-export default withAuth( function AddPhoto(props: any){
-    const dispatch = useDispatch();
+export default withAuth( function AddPhoto(props: any) {
+  const dispatch = useDispatch();
   const [form] = Form.useForm();
-  const onFinish = (values: any) => {
-    console.log("Received values of form: ", values);
-  };
   const [fileList, setFileList] = useState<UploadFile[]>([]);
   const [uploading, setUploading] = useState(false);
 
   const formik = useFormik({
     initialValues: {
       faphoFaciId: props.id,
-      faphoPrimary: false,
-      faphoUrl:undefined,
+      faphoPrimary: undefined,
+      faphoUrl: undefined,
     },
-    onSubmit: (values: any) => {
-      let payload = new FormData();
-      payload.append("faphoFaciId", values.faphoFaciId);
-      payload.append("faphoPrimary", values.faphoPrimary);
-      payload.append('faphoUrl[]', values.faphoUrl);
+    onSubmit: async (values: any) => {
+      const formData = new FormData();
+      formData.append("faphoFaciId", values.faphoFaciId);
+      formData.append("faphoPrimary", values.faphoPrimary);
 
-      dispatch(addFapho(payload));
-      props.closeUpload();
-      props.onRefresh();
-      window.location.reload();
+      for (let i = 0; i < values.faphoUrl.length; i++) {
+        formData.append("faphoUrl", values.faphoUrl[i]);
+      }
+
+      try {
+        setUploading(true);
+        await dispatch(addFapho(formData));
+        props.closeUpload();
+        props.onRefresh();
+        // window.location.reload();
+      } catch (error) {
+        console.log("Upload error:", error);
+      } finally {
+        setUploading(false);
+      }
     },
   });
-  console.log(fileList);
+
   const handleUpload = () => {
-    fileList.forEach((file:any) => {
-      formik.setFieldValue('faphoUrl[]', file as RcFile )
-      // formData.append('files[]', file as RcFile);
-    });
-    //setUploading(true);
-    formik.handleSubmit
-    // You can use any AJAX library you like
-    // fetch('https://www.mocky.io/v2/5cc8019d300000980a055e76', {
-      // method: 'POST',
-      // body: formData,
-    // })
-      // .then((res) => res.json())
-      // .then(() => {
-      //   setFileList([]);
-      //   message.success('upload successfully.');
-      // })
-      // .catch(() => {
-      //   message.error('upload failed.');
-      // })
-      // .finally(() => {
-      //   setUploading(false);
-      // });
+    formik.handleSubmit();
   };
 
   const upld: UploadProps = {
@@ -65,40 +52,65 @@ export default withAuth( function AddPhoto(props: any){
       const index = fileList.indexOf(file);
       const newFileList = fileList.slice();
       newFileList.splice(index, 1);
+      formik.setFieldValue("faphoUrl", newFileList);
       setFileList(newFileList);
     },
     beforeUpload: (file) => {
       setFileList([...fileList, file]);
-
+      formik.setFieldValue("faphoUrl", [...fileList, file]);
       return false;
     },
     fileList,
-    
   };
-  
-  
+  const stat = [
+    { value: true, label: "Primary" },
+    { value: false, label: "Not Primary" },
+  ];
+
   return (
     <Form
       form={form}
-      onFinish={onFinish}
+      onFinish={formik.handleSubmit}
       layout="vertical"
       style={{ maxWidth: 600 }}
-      className="px-5 py-5 border shadow rounded-lg mx-auto">
-        
-      <Upload {...upld}>
-        <Button icon={<UploadOutlined />}>Select File</Button>
-      </Upload>
-      <Button
-        type="primary"
-        onClick={handleUpload}
-        disabled={fileList.length === 0}
-        loading={uploading}
-        style={{ marginTop: 16 }}
-        
+      className="px-5 py-5 border shadow rounded-lg mx-auto"
+    >
+      <Form.Item
+        name="faphoPrimary"
+        label="Photo Status"
+        rules={[{ required: true, message: "Please input Hotel!" }]}
       >
-        {uploading ? "Uploading" : "Start Upload"}
-      </Button>
-      </Form>
+        <Select
+          placeholder="Photo Status}"
+          value={formik.values.faphoPrimary}
+          onChange={(value: any) => formik.setFieldValue("faphoPrimary", value)}
+        >
+          {stat.map((item: any, index: any) => (
+            <Select.Option key={index} value={item.value}>
+              {item.label}
+            </Select.Option>
+          ))}
+        </Select>
+      </Form.Item>
+
+      <Form.Item>
+        <Upload {...upld}>
+          <Button icon={<UploadOutlined />}>Select File</Button>
+        </Upload>
+      </Form.Item>
+
+      <Form.Item>
+        <Button
+          type="primary"
+          onClick={handleUpload}
+          disabled={fileList.length === 0}
+          loading={uploading}
+          style={{ marginTop: 16 }}
+        >
+          {uploading ? "Uploading" : "Start Upload"}
+        </Button>
+      </Form.Item>
+    </Form>
   );
 }
 )
